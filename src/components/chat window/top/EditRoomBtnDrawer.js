336ab37132423de87aec3b120e9b85d33df9c222
Anshul00739 <anshultrivedi00739@gmail.com>
2021-04-09 15:51:25 +0530

    update security rules for role-based permission

--- src/components/chat window/top/EditRoomBtnDrawer.js
+++ src/components/chat window/top/EditRoomBtnDrawer.js
@@ -1,21 +1,22 @@
 import React, { memo } from 'react';
+import { Button, Drawer, Alert } from 'rsuite';
 import { useParams } from 'react-router';
-import { Alert, Button, Drawer } from 'rsuite';
-import { useCurrentRoom } from '../../../context/current-room.context';
-import { useMediaQuery, useModalState } from '../../../misc/custom-hooks';
-import { database } from '../../../misc/firebase';
+import { useModalState, useMediaQuery } from '../../../misc/custom-hooks';
 import EditableInput from '../../EditableInput';
+import { useCurrentRoom } from '../../../context/current-room.context';
+import { database } from '../../../misc/firebase';
 
 const EditRoomBtnDrawer = () => {
   const { isOpen, open, close } = useModalState();
   const { chatId } = useParams();
   const isMobile = useMediaQuery('(max-width: 992px)');
+
   const name = useCurrentRoom(v => v.name);
   const description = useCurrentRoom(v => v.description);
 
   const updateData = (key, value) => {
     database
-      .ref(`room/${chatId}`)
+      .ref(`rooms/${chatId}`)
       .child(key)
       .set(value)
       .then(() => {
@@ -35,13 +36,14 @@ const EditRoomBtnDrawer = () => {
   };
 
   return (
-    <div>
+    <>
       <Button className="br-circle" size="sm" color="red" onClick={open}>
         A
       </Button>
+
       <Drawer full={isMobile} show={isOpen} onHide={close} placement="right">
         <Drawer.Header>
-          <Drawer.Title>Edit Rooom</Drawer.Title>
+          <Drawer.Title>Edit Room</Drawer.Title>
         </Drawer.Header>
         <Drawer.Body>
           <EditableInput
@@ -65,7 +67,7 @@ const EditRoomBtnDrawer = () => {
           </Button>
         </Drawer.Footer>
       </Drawer>
-    </div>
+    </>
   );
 };
 
